9a22621d52abf0f173b18a1888ec98dbddbde19a
Arto <airtonborges2211@gmail.com>
2022-10-19 11:14:33 +0000

    wip

--- src/app/game-window/game-window.component.ts
+++ src/app/game-window/game-window.component.ts
@@ -9,7 +9,27 @@ export class GameWindowComponent implements AfterViewInit {
 
   constructor() { }
   ngAfterViewInit(): void {
-    
+
+  }
+
+  createGame(window: any) {
+    var my_window = window as Window;
+    var buildUrl = "./assets/wasm/";
+    var loaderUrl = buildUrl + "Build.loader.js";
+    var script = my_window?.document.createElement("script");
+    script!.src = loaderUrl;
+    script.parentElement?.onload{
+      createUnityInstance(my_window?.document.querySelector("#unity-canvas"), {
+        dataUrl: "./assets/wasm/Build.data",
+        frameworkUrl: "./assets/wasm/Build.framework.js",
+        codeUrl: "./assets/wasm/Build.wasm",
+        productName: "Teste",
+      })
+      .then(this.onSuccess)
+      .catch(this.onError);
+    }
+  
+    my_window?.document.body.appendChild(script!);
   }
 
   private onSuccess() {
--- src/app/popout.directive.ts
+++ src/app/popout.directive.ts
@@ -1,15 +1,23 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Directive({
   selector: '[popout]'
 })
-export class PopoutDirective implements OnInit {
-  minha_window: Window | null = null;
+export class PopoutDirective implements OnInit{
+  @Output() createdWindow = new EventEmitter<Window | null>();
+
+  window: Window | null = null;
+  element: ElementRef | null = null;
+
   constructor(elementRef: ElementRef) { 
-    this.minha_window = window.open('', '_blank', 'popup');
+    this.element = elementRef; 
+  }
+
+  ngOnInit(): void {
+    this.window = window.open('', '_blank', 'popup');
     const styleSheetElement = document.createElement('link');
     document.querySelectorAll('link').forEach(htmlElement => {
-      if (htmlElement.rel === 'stylesheet') {
+      if (htmlElement.type === 'style') {
         const absoluteUrl = new URL(htmlElement.href).href;
         styleSheetElement.rel = 'stylesheet';
         styleSheetElement.href = absoluteUrl;
@@ -17,34 +25,9 @@ export class PopoutDirective implements OnInit {
     });
     console.log();
     
-    this.minha_window?.document.head.appendChild(styleSheetElement);
-    this.minha_window?.document.body.appendChild(elementRef.nativeElement);
+    this.window?.document.head.appendChild(styleSheetElement);
+    this.window?.document.body.appendChild(this.element?.nativeElement);
 
-  }
-  ngOnInit(): void {
-    var buildUrl = "./assets/wasm/";
-    var loaderUrl = buildUrl + "Build.loader.js";
-    var script = this.minha_window?.document.createElement("script");
-    script!.src = loaderUrl;
-    script!.onload = () => {
-      createUnityInstance(this.minha_window?.document.querySelector("#unity-canvas"), {
-        dataUrl: "./assets/wasm/Build.data",
-        frameworkUrl: "./assets/wasm/Build.framework.js",
-        codeUrl: "./assets/wasm/Build.wasm",
-        productName: "Teste",
-      })
-      .then(this.onSuccess)
-      .catch(this.onError());
-    };
-    this.minha_window?.document.body.appendChild(script!);
-  }
-
-  
-  private onSuccess() {
-    console.log('funcionou');
-  }
-
-  private onError() {
-    console.log('não funcionou');
+    this.createdWindow.emit(this.window);
   }
 }
